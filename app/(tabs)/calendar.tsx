7712df6cc38b9394ba2b0205ee82f5924aec7a0a
Jason-kosmos <jason@naxan-group.com>
2026-03-11 12:35:08 +0100

    fix

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useFocusEffect, useRouter } from 'expo-router';
 import React, { useCallback, useState } from 'react';
 import { Alert, FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
